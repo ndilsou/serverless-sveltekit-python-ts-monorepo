7b463e15487029158ff34b594f22f7f8e377336c
ndilsou <nasoungadoy@gmail.com>
2023-02-18 21:32:44 +0000

    fix issue with aws-lambda-rie

--- projects/apigw/lambda-entrypoint.sh
+++ projects/apigw/lambda-entrypoint.sh
@@ -4,4 +4,4 @@ if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
   exec /usr/local/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric $@
 else
   exec /usr/local/bin/python -m awslambdaric $@
-fi     
+fi
